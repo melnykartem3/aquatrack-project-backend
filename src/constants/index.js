4eec1b6346b1cccc2a91a4da40300c9d4cb01628
Danil <gul.danil99@gmail.com>
2024-08-03 03:49:20 +0300

    add sign up/in

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,5 @@
 export const emailRegexp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 export const genderTypeList = ['man', 'woman'];
+// для прикладу  нам потрібні обмежені в часі token, час можна змінити це так для зразка
+export const ACCESS_TOKEN_LIFETIME = 15 * 60 * 1000;
+export const REFRESH_TOKEN_LIFETIME = 3 * 24 * 3600 * 1000;
