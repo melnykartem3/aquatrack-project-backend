bc80a54f8913ec9a335b54c2f3f24e7a2fe15d3f
Artem Melnik <melnikartem@vesele-gimnaz.com>
2024-08-03 19:55:26 +0300

    remove comment

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
 export const emailRegexp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-export const genderTypeList = ['man', 'woman'];для зразка
+export const genderTypeList = ['man', 'woman']
 export const ACCESS_TOKEN_LIFETIME = 15 * 60 * 1000;
 export const REFRESH_TOKEN_LIFETIME = 3 * 24 * 3600 * 1000;
