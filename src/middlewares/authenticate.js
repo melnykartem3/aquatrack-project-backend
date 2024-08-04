79c2a24861473ea037d4021baf7171870b405a51
Artem Melnik <melnikartem@vesele-gimnaz.com>
2024-08-04 18:36:42 +0300

    .

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,45 +1,42 @@
-import createHttpError from "http-errors";
-import { findSession } from "../services/session.js";
-import { findUser } from "../services/auth.js";
-
-
+import createHttpError from 'http-errors';
+import { findSession } from '../services/session.js';
+import { findUser } from '../services/auth.js';
 
 export const authenticate = async (req, _, next) => {
-    const authHeader = req.get("Authorization");
+  const authHeader = req.get('Authorization');
 
-    if (!authHeader) {
-        return next(createHttpError(401, "Authorization header missing"));
-    }
+  if (!authHeader) {
+    return next(createHttpError(401, 'Authorization header missing'));
+  }
 
-    const [bearer, accessToken] = authHeader.split(" ");
+  const [bearer, accessToken] = authHeader.split(' ');
 
-    if (bearer !== "Bearer") {
-        return next(createHttpError(401, "Token must have Bearer type"));
-    }
+  if (bearer !== 'Bearer') {
+    return next(createHttpError(401, 'Token must have Bearer type'));
+  }
 
-    if (!accessToken) {
-        return next(createHttpError(401, "token is missing"));
-    }
+  if (!accessToken) {
+    return next(createHttpError(401, 'token is missing'));
+  }
 
-    const session = await findSession({ accessToken });
+  const session = await findSession({ accessToken });
 
-    if (!session) {
-        return next(createHttpError(401, "session not found"));
-    }
+  if (!session) {
+    return next(createHttpError(401, 'session not found'));
+  }
 
-    const accessTokenExpired = new Date() > new Date(session.accessTokenValidUntil);
+  const accessTokenExpired =
+    new Date() > new Date(session.accessTokenValidUntil);
 
-    if (accessTokenExpired) {
-        return next(createHttpError(401, "Access token expired"));
-    }
+  if (accessTokenExpired) {
+    return next(createHttpError(401, 'Access token expired'));
+  }
 
-    const user = await findUser({ _id: session.userId });
+  const user = await findUser({ _id: session.userId });
 
-    if (!user) {
-        return next(createHttpError(401, "user not found"));
-    }
-    req.user = user;
-    next();
+  if (!user) {
+    return next(createHttpError(401, 'user not found'));
+  }
+  req.user = user;
+  next();
 };
-};
-
