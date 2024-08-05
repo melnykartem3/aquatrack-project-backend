c6944eff23886fc4181afac76870758fa737d6a4
Artem Melnik <melnikartem@vesele-gimnaz.com>
2024-08-06 00:03:53 +0300

    edit router

--- src/routers/water.js
+++ src/routers/water.js
@@ -11,8 +11,6 @@ import { createWaterSchema, updateWaterSchema } from '../validation/water.js';
 import { isValidId } from '../middlewares/isValidId.js';
 import { createNewWaterController } from '../controllers/water.js';
 
-import { authenticate } from '../middlewares/authenticate.js';
-
 import {
   getWaterPerDayController,
   getWaterPerMonthController,
@@ -22,11 +20,19 @@ const waterRouter = Router();
 
 waterRouter.use(authenticate);
 
-waterRouter.post('/', validateBody(createWaterSchema), ctrlWrapper(createNewWaterController));
+waterRouter.post(
+  '/',
+  validateBody(createWaterSchema),
+  ctrlWrapper(createNewWaterController),
+);
 
 waterRouter.put('/:waterId', validateBody(), ctrlWrapper());
 
-waterRouter.patch('/:waterId', ctrlWrapper(updateWaterController));
+waterRouter.patch(
+  '/:waterId',
+  validateBody(updateWaterSchema),
+  ctrlWrapper(updateWaterController),
+);
 
 waterRouter.delete('/:waterId', ctrlWrapper(deleteWaterController));
 
@@ -35,11 +41,11 @@ waterRouter.get('/perDay', ctrlWrapper(getWaterPerDayController));
 waterRouter.get('/perMonth', ctrlWrapper(getWaterPerMonthController));
 
 waterRouter.use('*', (req, res, next) => {
-    res.status(404).json({
-        status: 404,
-        message: ' record not found',
-    });
-    next();
+  res.status(404).json({
+    status: 404,
+    message: ' record not found',
+  });
+  next();
 });
 
 export default waterRouter;
