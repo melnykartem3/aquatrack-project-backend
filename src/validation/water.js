a519fc661415ea039973ef5a98690f4bb81c6962
Artem Melnik <melnikartem@vesele-gimnaz.com>
2024-08-06 01:18:08 +0300

    change water schema

--- src/validation/water.js
+++ src/validation/water.js
@@ -1,13 +1,11 @@
-import Joi from "joi";
+import Joi from 'joi';
 
 export const createWaterSchema = Joi.object({
-    date: Joi.string(),
-    waterVolume: Joi.string(),
-
+  date: Joi.string(),
+  waterVolume: Joi.number(),
 });
 
 export const updateWaterSchema = Joi.object({
-   date: Joi.string(),
-    waterVolume: Joi.string(),
+  date: Joi.string(),
+  waterVolume: Joi.number(),
 });
-
